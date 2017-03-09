8c35b8b7367d255cf2de13fb280a81671fe638d7
Aris Jirat <arisjiratkurniawan@gmail.com>
2017-03-09 14:43:24 +0700

    some

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 var fs = require('fs');
 var path = require('path');
+var glob = require('glob');
 var source = 'data/';
 
 fs.readdir(source, (err, files) => {
 	files.forEach(file => {
-		fs.readdir(source + file + '/', (err, subFiles) => {
-			// file are parent folder name we trace
-			subFiles.forEach(subFile => {
-				// define prefix name
-				// find extension of subFiles
-				// to know images is
-				var prefix = file+'_';
-				var ext = path.extname(source + file + '/' + subFile);
-				var image = (ext === '.jpg' || ext === '.jpeg'|| ext === '.png' || ext === '.svg' || ext === '.gif') ? true : false;
-				var pathOld = source + file + '/' + subFile;
-				var pathNew = source + file + '/' + file +'_'+ subFile;
-				// check if subFile not have exist prefix
-				if(!subFile.startsWith(prefix)) {
-					if(image) {
-						fs.rename(pathOld, pathNew, (err, done) => {
-							if(err) throw err;
-						});
-					}
-				}
-			});
+		var prefix = file+'_';
+
+		glob(source + file +'/**/*.jpg', function (err, subFiles) {
+			if(subFiles.length>0) {
+				subFiles.forEach(getFile => {
+					var dirname = path.dirname(getFile);
+					var oldFileName = dirname + '/' + path.basename(getFile);
+					var newFileName = dirname + '/' + prefix + path.basename(getFile);
+					fs.rename(oldFileName, newFileName, (err, done) => {
+						if(err) throw err;
+					});
+				});
+			}
 		});
+
 	});
 });
 
